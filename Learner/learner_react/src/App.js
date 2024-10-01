65b85bc577b994e868658d9ffdbf21034482d13c
gahyeon <kahyun1890@gmail.com>
2024-10-01 11:49:46 +0900

    Update React UI Course ing...

--- Learner/learner_react/src/App.js
+++ Learner/learner_react/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Courses from "./pages/Courses";
+import CourseDetail from "./pages/CourseDetail";
+import SignUp from "./pages/SignUp";
+import LoginModal from "./components/LoginModal";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
+    return (
+        <Router>
+            <Header openModal={openModal} />
+            {isModalOpen && <LoginModal closeModal={closeModal} />}
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/courses" element={<Courses />} />
+                <Route path="/courses/:courseId" element={<CourseDetail />} />
+                <Route path="/signup" element={<SignUp />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
--- Learner/src/main/java/edu/example/learner/courseabout/course/controller/CourseController.java
+++ Learner/src/main/java/edu/example/learner/courseabout/course/controller/CourseController.java
@@ -14,6 +14,7 @@ import java.util.Map;
 @RestController
 @Log4j2
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/v1/course")
 public class CourseController {
     private final CourseService courseService;
